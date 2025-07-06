963f12724270591ef32a8c1042952f252a430ec5
Pranshu Sheoran <sheoranpranshu@gmail.com>
2025-07-06 18:53:01 +0530

    Update setup_ubuntu_24.sh

--- setup_ubuntu_24.sh
+++ setup_ubuntu_24.sh
@@ -69,30 +69,37 @@ main() {
     run_task "Updating package lists" apt-get update -y
     run_task "Upgrading existing packages" apt-get upgrade -y
     
-    # All packages in one list for a single, efficient install command
-    local aosp_packages="bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick \
-    lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync \
-    schedtool squashfs-tools xsltproc zip zlib1g-dev gh iotop bashtop nethogs autossh apache2 p7zip neofetch gettext autoconf \
-    automake libtool make gcc autopoint patchelf python-is-python3 clang lld llvm gcc-multilib zsh openjdk-17-jdk openjdk-17-jre \
-    xmlstarlet micro"
+    # <-- FIX 1: The package list is now a bash array.
+    # This ensures each package name is passed as a separate argument to apt-get.
+    local aosp_packages=(
+        bc bison build-essential ccache curl flex g++-multilib gcc-multilib git
+        git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev
+        liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush
+        rsync schedtool squashfs-tools xsltproc zip zlib1g-dev gh iotop bashtop
+        nethogs autossh apache2 p7zip neofetch gettext autoconf automake libtool
+        make gcc autopoint patchelf python-is-python3 clang lld llvm gcc-multilib
+        zsh openjdk-17-jdk openjdk-17-jre xmlstarlet micro
+    )
 
-    run_task "Installing all AOSP build dependencies" apt-get install -y $aosp_packages
+    # <-- FIX 2: The array is expanded correctly with "${aosp_packages[@]}".
+    run_task "Installing all AOSP build dependencies" apt-get install -y "${aosp_packages[@]}"
 
     # --- Phase 2: Manual & System Configurations ---
     echo -e "\n${C_GREEN}Phase 2: System Configuration${C_NC}"
     
-    # Manual install of ncurses5
+    # Manual install of ncurses5 for compatibility with older build systems
     local libtinfo_url="http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb"
     local libncurses_url="http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.3-2_amd64.deb"
-    run_task "Manually installing libtinfo5" "sh -c 'wget -q -O /tmp/libtinfo5.deb $libtinfo_url && dpkg -i /tmp/libtinfo5.deb && rm /tmp/libtinfo5.deb'"
-    run_task "Manually installing libncurses5" "sh -c 'wget -q -O /tmp/libncurses5.deb $libncurses_url && dpkg -i /tmp/libncurses5.deb && rm /tmp/libncurses5.deb'"
+    
+    # <-- FIX 3: Commands are now passed as separate arguments, not a single quoted string.
+    run_task "Manually installing libtinfo5" sh -c "wget -q -O /tmp/libtinfo5.deb $libtinfo_url && dpkg -i /tmp/libtinfo5.deb && rm /tmp/libtinfo5.deb"
+    run_task "Manually installing libncurses5" sh -c "wget -q -O /tmp/libncurses5.deb $libncurses_url && dpkg -i /tmp/libncurses5.deb && rm /tmp/libncurses5.deb"
 
     # Install repo tool system-wide
-    run_task "Installing 'repo' tool system-wide" "sh -c 'curl -s https://storage.googleapis.com/git-repo-downloads/repo -o /usr/bin/repo && chmod a+x /usr/bin/repo'"
+    run_task "Installing 'repo' tool system-wide" sh -c "curl -s https://storage.googleapis.com/git-repo-downloads/repo -o /usr/bin/repo && chmod a+x /usr/bin/repo"
 
-    # Disable AppArmor setting
-    local apparmor_conf_cmd="echo 'kernel.apparmor_restrict_unprivileged_userns = 0' | tee /etc/sysctl.d/20-apparmor-donotrestrict.conf"
-    run_task "Configuring AppArmor rule" "sh -c '$apparmor_conf_cmd'"
+    # Disable AppArmor user namespace restrictions required for sandboxed builds
+    run_task "Configuring AppArmor rule" sh -c "echo 'kernel.apparmor_restrict_unprivileged_userns = 0' > /etc/sysctl.d/20-apparmor-donotrestrict.conf"
     run_task "Applying AppArmor rule immediately" sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 
 
